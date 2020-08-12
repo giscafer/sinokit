6f75feeb3a469d439d0785823a5ad7eb2afe3d2d
giscafer <giscafer@outlook.com>
2020-08-12 18:20:37 +0800

    chore: 时间抽滚动和缩放研究

--- app/views/timeline/index.vue
+++ app/views/timeline/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>时间轴</h3>
+    <el-button type="primary" @click="handleZoom">缩放</el-button>
     <div id="visualization"></div>
     <svg width="960" height="50" />
   </div>
@@ -13,13 +14,30 @@ export default {
   name: 'TimelinePage',
   props: {},
   data() {
-    return { timeline: null }
+    return {
+      timeline: null,
+      zoomRange: 0,
+      brushRange: 0,
+      startDate: new Date(),
+      endDate: new Date(),
+      toggle: false,
+    }
   },
   created() {},
   mounted() {
     this.createTimeline()
   },
   methods: {
+    handleZoom() {
+      let percent = Math.random().toFixed(2)
+      if (this.toggle) {
+        this.timeline.zoomIn(+percent)
+      } else {
+        this.timeline.zoomOut(+percent)
+      }
+      console.log(this.toggle)
+      this.toggle = !this.toggle
+    },
     createTimeline() {
       var container = document.getElementById('visualization')
 
@@ -66,10 +84,16 @@ export default {
             '<div><p class="date">2004-11-18</p><a href="" target="_blank">上市</a></div>',
         },
       ])
-
+      const minDate = new Date(2009, 1, 1)
+      const maxDate = new Date(2018, 1, 1)
+      const zoomMax = maxDate.getTime() - minDate.getTime()
+      const zoomMin = 31104000000 * 3 // 季度为缩放单位
+      this.startDate = minDate
+      this.endDate = maxDate
+      console.log(zoomMax)
       var options = {
-        // zoomMin: 10,
-        // zoomMax: 30,
+        zoomMin,
+        zoomMax,
         orientation: {
           axis: 'top',
           item: 'top',
@@ -78,10 +102,21 @@ export default {
           item: 20,
           axis: 20,
         },
+        // zoomFriction: 10,
+        // zoomable: false,
+        max: maxDate,
+        min: minDate,
+        timeAxis: { scale: 'year', step: 1 },
       }
-
       this.timeline = new Timeline(container, items, options)
+      window.timeline = this.timeline
+      this.zoomRange = zoomMax - zoomMin
+      console.log('this.zoomRange=', this.zoomRange)
 
+      // timeline.fit() 自适应
+      // timeline.zoomIn() // 放大
+      // timeline.zoomOut() // 缩小
+      // timeline.moveTo() // 移动到
       this.brushInit()
     },
     brushInit() {
@@ -94,7 +129,7 @@ export default {
       const width = targetRect.width
       // const x2 = d3.scaleTime().range([0, width]);
       const height = 20
-      console.log(width, height)
+      // console.log(width, height)
       const x = d3.scaleTime().range([0, width])
       const context = svg.append('g').attr('class', 'context')
 
@@ -124,20 +159,38 @@ export default {
               : (d, i) => `translate(${selection[i]},${height / 2})`
           )
       }
+      let brushedSelection = this.brushRange[1] / 2
 
+      const moveTo = ({ selection, type }) => {
+        // brush 宽度变化时动态缩放时间轴
+        if (type === 'brush') {
+          const w = selection[1] - selection[0]
+          const rate = +(w / width).toFixed(2)
+          if (w < brushedSelection) {
+            this.timeline.zoomOut(rate)
+            console.info('zoomOut', rate)
+          } else if (w > brushedSelection) {
+            this.timeline.zoomIn(rate)
+            console.info('zoomIn', rate)
+          }
+          brushedSelection = w
+        }
+        const position = selection[1]
+        const newPosigion = this.zoomRange * (position / this.brushRange[1])
+        console.log(position, newPosigion, this.brushRange[1], this.zoomRange)
+        const date = new Date(this.startDate.getTime() + newPosigion)
+        console.log(date)
+        setTimeout(() => {
+          // 需要区分 宽度变化还是滑动，只能执行一个方法，不然 zoomIn 和 zoomOut 不生效
+          // this.timeline.moveTo(date)
+        })
+      }
       const brushed = () => {
         if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'zoom') return // ignore brush-by-zoom
         var s = d3.event.selection
-        console.log(s)
-        /*  x.domain(s.map(x2.invert, x2))
+        // console.log('evt=', d3.event)
 
-        focus.select('.area').attr('d', area)
-        focus.select('.axis--x').call(xAxis)
-
-        svg.call(
-          zoom.transform,
-          d3.zoomIdentity.scale(width / (s[1] - s[0])).translate(-s[0], 0)
-        ) */
+        moveTo(d3.event)
 
         d3.select('svg').call(brushHandle, s)
       }
@@ -149,23 +202,14 @@ export default {
           [width, height],
         ])
         .on('brush end', brushed)
-
+      this.brushRange = x.range()
+      const brushWidth = this.brushRange[1] / 2 // 初始化的长度
       context
         .append('g')
         .attr('class', 'brush')
         .call(brush)
-        .call(brush.move, x.range())
-
-      /*  context
-        .select('.brush')
-        .append('path')
-        .attr('class', 'handle--custom')
-        .attr('fill', '#666')
-        .attr('fill-opacity', 0.8)
-        .attr('stroke', '#000')
-        .attr('stroke-width', 1.5)
-        .attr('cursor', 'ew-resize')
-        .attr('d', arc()) */
+        .call(brush.move, [this.brushRange[0], this.brushRange[1] / 2])
+      this.timeline.zoomIn(brushWidth / this.brushRange[1])
     },
   },
 }
