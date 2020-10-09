f84ef4e2ff3c0e2efee47d938f4f8f32e2332440
giscafer <giscafer@outlook.com>
2020-10-09 15:30:48 +0800

    chore: del json schema editor file

--- packages/components/src/json-schema-editor/index.js
+++
@@ -1,8 +0,0 @@
-import JsonSchemaEditor from './src/json-schema-editor';
-
-/* istanbul ignore next */
-JsonSchemaEditor.install = function(Vue) {
-  Vue.component(JsonSchemaEditor.name, JsonSchemaEditor);
-};
-
-export default JsonSchemaEditor;
